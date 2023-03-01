e15b3af9a7cf431bfb1d5f816eff8c72e90867cd
Mahendra <mahendramistri@gmail.com>
2023-03-01 12:21:54 +0530

    add required changes

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello World! %d", add(4, 5));
+    }
+
+    public static int add(int n1, int n2) {
+        return n1 + n2;
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.example.App;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        int result = App.add(3, 4);
+        assertEquals(7, result);
+    }
+}
