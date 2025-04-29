8b2badf538c0e225fd0d4ecdb4df689f97ca54e4
Mahendra <mahendramistri@gmail.com>
2025-04-29 17:43:11 +0530

    new changes

--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -1,2 +1,25 @@
-package com.test;public class BaseTest {
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    WebDriver driver;
+   @BeforeClass
+   public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\32bit\\chromedriver-win32\\chromedriver.exe");
+       driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+       driver.close();
+      // driver.quit();
+    }
+
+    public void refreshPage() {
+       driver.navigate().refresh();
+    }
 }
